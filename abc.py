edaf3ab435fed9b82a46875dfe8d04323e9cdb22
karthi-112 <56634311+karthi-112@users.noreply.github.com>
2022-06-09 19:54:38 +0530

    abc.py

--- abc.py
+++ abc.py
@@ -1,7 +1,4 @@
-import math as M  
-Radius = float (input ("Please enter the radius of the given circle: "))  
-area_of_the_circle = M.pi* Radius * Radius  
-print (" The area of the given circle is: ", area_of_the_circle) 
-filename = input("Input the Filename: ")
-f_extns = filename.split(".")
-print ("The extension of the file is : " + repr(f_extns[-1]))
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
